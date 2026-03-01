298abaa299610982299bb2c0c73514887a282a6c
IAN ROOT <ianaroot@gmail.com>
2026-03-01 18:45:32 -0500

    various minor bug fixes -make all bots display in index regardless of user presence -add db index for node connections to reinforce uniqueness and increase lookup speeds -modernize delete with turbo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_03_01_025349) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_01_181000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_03_01_025349) do
     t.bigint "target_node_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["source_node_id", "target_node_id"], name: "index_node_connections_on_source_node_id_and_target_node_id", unique: true
     t.index ["source_node_id"], name: "index_node_connections_on_source_node_id"
     t.index ["target_node_id"], name: "index_node_connections_on_target_node_id"
   end
