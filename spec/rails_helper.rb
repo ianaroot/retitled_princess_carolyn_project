56c5e90a2c577ec30c732c80b5d6781f1c7643dd
IAN ROOT <ianaroot@gmail.com>
2026-03-04 19:29:04 -0500

    database_cleaner-active_record gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,6 +24,7 @@ Capybara.javascript_driver = :selenium_chrome
 Capybara.default_max_wait_time = 5
 
 # Add additional requires below this line. Rails is not loaded until this point!
+require_relative 'support/database_cleaner'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
