a3bb6d898d01309f2f805427481bf4b818d7c3d2
IAN ROOT <ianaroot@gmail.com>
2026-03-19 13:45:18 -0400

    fix merge in migration

--- db/migrate/20251219003000_add_name_and_description_to_bots.rb
+++ db/migrate/20251219003000_add_name_and_description_to_bots.rb
@@ -16,9 +16,6 @@ class CreateGames < ActiveRecord::Migration[7.1]
     end
   end
 end
-ianroot:retitled_princess_carolyn_project (main) % git show a2a3585:db/migrate/2025121900
-fatal: path 'db/migrate/2025121900' does not exist in 'a2a3585'
-ianroot:retitled_princess_carolyn_project (main) % git show a2a3585:db/migrate/20251219003000_add_name_and_description_to_bots.rb
 class AddNameAndDescriptionToBots < ActiveRecord::Migration[7.1]
   def change
     add_column :bots, :name, :string
